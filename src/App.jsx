c609503e955bbf22041865af142dc60ac9ab16d3
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-12-03 17:18:29 +0700

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './login.css';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [name, setName] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(name.length === 4) {
+            navigate('/home')
+        }
+        else alert('Mohon masukan 4 angka')
+    };
+
     return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
+        <Container>
+            <div className="login-container">
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3 login-content" controlId="formBasicEmail">
+                        <h1 className='login-title'>Todo App</h1>
+                        <Form.Label>Login ?</Form.Label>
+                        <Form.Control type="number" placeholder="Masukan 4 angka" onChange={(e) => setName(e.target.value)}/>
+                        <Button variant="primary" type="submit" className='login-button'>Submit</Button>
+                    </Form.Group>
+                </Form>
+            </div>
+        </Container>
     )
 }
 
