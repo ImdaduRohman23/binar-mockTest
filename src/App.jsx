e292df931c6d34904d0a41d6e3c03f6508ad65b1
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-12-03 16:40:19 +0700

    add pages and set router

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/home' element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
